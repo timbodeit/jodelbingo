ebb97d23190f209838a44175009e4de2952eefd1
Tim Bodeit <tim@bodeit.com>
2016-11-02 19:59:13 +0100

    Add Jodel Squares and ajust headline/footer

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 
-var headerText = "Bingo";
+var headerText = "Jodel Bingo";
 
-var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
+var footerText = "<a href='https://github.com/timbodeit/jodelbingo/' target='_blank'>Code available on GitHub</a><br><a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Based on HTML5-bingo by jeffehobbs</a>";
 
 var winText = "Winner";
 
@@ -9,36 +9,35 @@ var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
-var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
-    ]
-};
+var data = [
+  "Kik?",
+  "M oder W?",
+  "Jeder, immer",
+  "Kontrollörres",
+  "Leierkasten",
+  "Paulaner",
+  "W21, nackt",
+  "Müsste ich nackt sehen",
+  "Justus",
+  "Lörres reinhämmern",
+  "Manni",
+  "Dumm wie Aachen",
+  "Rote Schuhe",
+  "Grundschul-mausi",
+  "Unglücklicher Single",
+  "Ronni",
+  "Mimimi",
+  "Fotojodel",
+  "Gadse",
+  "#flirtenkannich",
+  "Bellgadse",
+  "Sonstige Kadse",
+  "Lisa",
+  "Laura",
+  "#jhj",
+  "das/dass seid/seit einzigste",
+  "#dankemerkel",
+  "memax92"
+]
+
+var JSONBingo = {"squares": data.map(function(e){ return {"square": e} })};
